d5219f62d10fd1300adbd8c3bb9e250b7a2577c7
Max Zuev <mail@thenno.me>
2019-01-11 18:09:46 +0300

    Add tests for line_of_view

---
+++ tests/core/test_board.py
@@ -0,0 +1,31 @@
+from core.board import (
+    Position,
+    line_of_view,
+)
+
+
+def test_line_of_view():
+    assert list(line_of_view(Position(5, 5), Position(0, 0))) == [
+        Position(x=5, y=5),
+        Position(x=4, y=4),
+        Position(x=3, y=3),
+        Position(x=2, y=2),
+        Position(x=1, y=1),
+        Position(x=0, y=0),
+    ]
+    assert list(line_of_view(Position(5, 5), Position(10, 0))) == [
+        Position(x=5, y=5),
+        Position(x=6, y=4),
+        Position(x=7, y=3),
+        Position(x=8, y=2),
+        Position(x=9, y=1),
+        Position(x=10, y=0),
+    ]
+    assert list(line_of_view(Position(5, 5), Position(10, 5))) == [
+        Position(x=5, y=5),
+        Position(x=6, y=5),
+        Position(x=7, y=5),
+        Position(x=8, y=5),
+        Position(x=9, y=5),
+        Position(x=10, y=5),
+    ]
