f88c2cd73babba715a09ebbbccf61d280664669f
Max Zuev <mail@thenno.me>
2019-01-11 18:16:10 +0300

    Another test for line_of_view

--- tests/core/test_board.py
+++ tests/core/test_board.py
@@ -29,3 +29,13 @@ def test_line_of_view():
         Position(x=9, y=5),
         Position(x=10, y=5),
     ]
+    assert list(line_of_view(Position(8, 5), Position(1, 1))) == [
+        Position(x=8, y=5),
+        Position(x=7, y=4),
+        Position(x=6, y=4),
+        Position(x=5, y=3),
+        Position(x=4, y=3),
+        Position(x=3, y=2),
+        Position(x=2, y=2),
+        Position(x=1, y=1),
+    ]
