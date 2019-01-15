014bd2ff29a08b95d2959ebca268814a8870bf66
Max Zuev <mail@thenno.me>
2019-01-15 03:00:38 +0300

    Rename project

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(
-        name='epic_battle',
+        name='ZPGEngine',
         packages=['core'],
         package_dir={'': 'src'},
         version='0.0.1',
         author='sir Max',
-        author_email='mail@thenno.me',
         include_package_data=True,
     )
