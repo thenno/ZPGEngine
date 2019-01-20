d78ae7a2b47602e90c081f9b2071dc671f6123cf
Max Zuev <mail@thenno.me>
2019-01-21 01:03:05 +0300

    Take into account memory in AI

--- src/core/ai.py
+++ src/core/ai.py
@@ -1,7 +1,28 @@
+from typing import Set
+
 from core.randomizer import Randomizer
-from core.actions import Command, Commands
+from core.actions import Command, Commands, CommandWalk
 from core.game import MarineKnowledge
+from core.board import FOV
 
 
 def choose_command(commands: Commands, knowledge: MarineKnowledge, randomizer: Randomizer) -> Command:
-    return randomizer.choice(list(commands))
+    # simplify implementation
+    # we don't have anything except Walk
+    commands = Commands(set(filter(lambda x: isinstance(x, CommandWalk), commands)))
+    command = _choose_walk(commands, knowledge, randomizer)  # type: ignore
+    return command
+
+
+def _choose_walk(commands: Set[CommandWalk], knowledge: MarineKnowledge, randomizer: Randomizer) -> CommandWalk:
+    commands = list(commands)
+    if len(commands) == 1:
+        return commands[0]
+    for command in commands:
+        if command.pos_to not in knowledge.memory.way:
+            return command
+    return randomizer.choice(commands)
+
+
+def _is_enemy_in_fov(fov: FOV):
+    return False
