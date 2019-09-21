fd92320db706374997cfda7d20f8087807378d76
Max Zuev <mail@thenno.me>
2019-09-21 04:49:16 +0300

    Fix component's filter by 2 or more elements

--- main.py
+++ main.py
@@ -23,10 +23,15 @@ class ComponentManager:
         self._components = components
 
     def filter(self, component_classes):
+        result = None
         for component_class in component_classes:
-            for entity, _ in enumerate(self._components[component_class]):
-                if self._components[component_class][entity] is not None:
-                    yield entity
+            entities = {
+                entity
+                for entity, _ in enumerate(self._components[component_class])
+                if self._components[component_class][entity] is not None
+            }
+            result = entities if result is None else result & entities
+        yield from result
 
     def get(self, entity, component_class):
         return self._components[component_class][entity]
